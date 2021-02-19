fc3e5cf940d78164d76227afb352ac418007149e
Poseydon42 <vvmposeydon@gmail.com>
2021-02-20 00:23:36 +0200

    Windows lauch now displays error message box when it fails to either load game DLL or get address of CreateApplicationInstance()

--- Hermes/Source/Core/Launch/LaunchWindows.cpp
+++ Hermes/Source/Core/Launch/LaunchWindows.cpp
@@ -3,20 +3,33 @@
 #include <windows.h>
 #include <string>
 
-typedef void(*InitGame)();
+#include "Core/Application/Application.h"
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
 	std::wstring GameModuleName = TEXT(HERMES_GAME_NAME);
 	GameModuleName += L".dll";
 	HMODULE GameModule = LoadLibrary(GameModuleName.c_str());
 	if (GameModule)
 	{
-		InitGame Func = (InitGame)GetProcAddress(GameModule, "InitGame");
-		if (Func)
+		Hermes::CreateApplicationInstance CreateApplicationInstanceImpl = (Hermes::CreateApplicationInstance)GetProcAddress(GameModule, "CreateApplicationInstance");
+		if (CreateApplicationInstanceImpl)
 		{
-			Func();
+			Hermes::IApplication* App = CreateApplicationInstanceImpl();
+		}
+		else
+		{
+			MessageBoxW(0, L"Failed to get address of function CreateApplicationInstance", L"Error loading game module", MB_ICONHAND | MB_OK);
 		}
 	}
+	else
+	{
+		std::wstring ErrorText = L"Failed to load ";
+		ErrorText += GameModuleName;
+		MessageBoxW(0, ErrorText.c_str(), L"Error loading game module", MB_ICONHAND | MB_OK);
+	}
+
+	return 0;
 }
+
 #endif

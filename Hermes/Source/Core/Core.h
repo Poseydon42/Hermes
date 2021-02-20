58aecdfbcc8a795844500ad28ea0b6b1fb18bd3b
Poseydon42 <vvmposeydon@gmail.com>
2021-02-20 19:17:55 +0200

    Typedefs for all basic data types

--- Hermes/Source/Core/Core.h
+++ Hermes/Source/Core/Core.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <stdint.h>
 
 #include "Core/Compiler/CompilerMSVC.h"
 
@@ -11,6 +12,17 @@ namespace Hermes
 	 * ANSI strings are supported only for accessing 3rd party code and API
 	 */
 	using String = std::wstring;
+
+	typedef uint8_t uint8;
+	typedef int8_t int8;
+	typedef uint16_t uint16;
+	typedef int16_t int16;
+	typedef uint32_t uint32;
+	typedef int32_t int32;
+	typedef uint64_t uint64;
+	typedef int64_t int64;
+
+	typedef size_t uint64;
 
 #ifdef HERMES_BUILD_ENGINE
 #define HERMES_API API_EXPORT

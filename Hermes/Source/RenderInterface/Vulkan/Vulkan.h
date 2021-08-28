6bad30fcf2f29f3de84879c52ef71caa8c8bb050
Poseydon42 <vvmposeydon@gmail.com>
2021-08-28 13:13:45 +0300

    Vulkan: Used SUPPRESS_ALL_WARNINGS_BEGIN/END in Vulkan.h to suppress warnings from Vulkan and VulkanMemoryAllocator include files

--- Hermes/Source/RenderInterface/Vulkan/Vulkan.h
+++ Hermes/Source/RenderInterface/Vulkan/Vulkan.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include "Core/Core.h"
+
 #ifdef HERMES_PLATFORM_WINDOWS
 #define VK_USE_PLATFORM_WIN32_KHR
 #endif
 // !!!!! DON'T EXCHANGE THOSE TWO INCLUDES. vk_mem_alloc.h causes lot's of build error if it is included after Vulkan headers !!!!!
+SUPPRESS_ALL_WARNINGS_BEGIN
 #include <vk_mem_alloc.h>
 #include <vulkan/vulkan.h>
-
-#include "Core/Core.h"
+SUPPRESS_ALL_WARNINGS_END
 
 namespace Hermes::Vulkan
 {

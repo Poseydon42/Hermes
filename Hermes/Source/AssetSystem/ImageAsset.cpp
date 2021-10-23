8655fd41d91f21ba86b340f84b497c97a8089a36
Poseydon42 <vvmposeydon@gmail.com>
2021-10-23 23:46:20 +0300

    AssetSystem: Implemented ImageAsset

---
+++ Hermes/Source/AssetSystem/ImageAsset.cpp
@@ -0,0 +1,80 @@
+﻿#include "ImageAsset.h"
+
+#include <algorithm>
+
+namespace Hermes
+{
+	uint8 BytesPerPixelForImageFormat(ImageFormat Format)
+	{
+		switch (Format)
+		{
+		case ImageFormat::R8:
+			return 8;
+		case ImageFormat::R16:
+		case ImageFormat::R8G8:
+			return 16;
+		case ImageFormat::B8G8R8X8:
+		case ImageFormat::B8G8R8A8:
+		case ImageFormat::R16G16:
+		case ImageFormat::R32:
+			return 32;
+		default:
+			HERMES_ASSERT(false);
+		}
+		return 0;
+	}
+
+	ImageAsset::ImageAsset(const String& Name, Vec2ui InDimensions, ImageFormat InFormat)
+		: Asset(Name, AssetType::Image)
+		, Dimensions(InDimensions)
+		, Format(InFormat)
+	{
+		Data.resize(GetMemorySize(), 0x00);
+	}
+
+	ImageAsset::ImageAsset(const String& Name, Vec2ui InDimensions, ImageFormat InFormat, const uint8* InData)
+		: Asset(Name, AssetType::Image)
+		, Dimensions(InDimensions)
+		, Format(InFormat)
+	{
+		Data.resize(GetMemorySize(), 0x00);
+		std::copy_n(InData, GetMemorySize(), Data.data());
+	}
+
+	bool ImageAsset::IsValid() const
+	{
+		return (Data.size() == Dimensions.X * Dimensions.Y * GetBitsPerPixel() / 8);
+	}
+
+	size_t ImageAsset::GetMemorySize() const
+	{
+		return static_cast<size_t>(Dimensions.X) * Dimensions.Y * GetBitsPerPixel() / 8;
+	}
+
+	const uint8* ImageAsset::GetRawData() const
+	{
+		HERMES_ASSERT(!Data.empty());
+		return Data.data();
+	}
+
+	uint8* ImageAsset::GetRawData()
+	{
+		HERMES_ASSERT(!Data.empty());
+		return Data.data();
+	}
+
+	Vec2ui ImageAsset::GetDimensions() const
+	{
+		return Dimensions;
+	}
+
+	ImageFormat ImageAsset::GetImageFormat() const
+	{
+		return Format;
+	}
+
+	uint8 ImageAsset::GetBitsPerPixel() const
+	{
+		return BytesPerPixelForImageFormat(Format);
+	}
+}
---
+++ Hermes/Source/AssetSystem/ImageAsset.h
@@ -0,0 +1,63 @@
+﻿#pragma once
+
+#include <memory>
+
+#include "Core/Core.h"
+#include "AssetSystem/Asset.h"
+#include "Math/Vector2.h"
+
+namespace Hermes
+{
+	class AssetLoader;
+
+	// TODO : maybe add more?
+	enum class ImageFormat : uint8
+	{
+		R8 = 0x01,
+		R16 = 0x02,
+		R32 = 0x03,
+		R8G8 = 0x05,
+		R16G16 = 0x0A,
+		B8G8R8X8 = 0x54,
+		B8G8R8A8 = 0x55
+	};
+
+	uint8 BytesPerPixelForImageFormat(ImageFormat Format);
+
+	class HERMES_API ImageAsset final : public Asset
+	{
+	public:
+
+		virtual bool IsValid() const override;
+
+		virtual size_t GetMemorySize() const override;
+
+		const uint8* GetRawData() const;
+		uint8* GetRawData();
+
+		Vec2ui GetDimensions() const;
+
+		ImageFormat GetImageFormat() const;
+
+		uint8 GetBitsPerPixel() const;
+
+	private:
+		/*
+		 * Constructor that initializes raw data to 0
+		 */
+		ImageAsset(const String& Name, Vec2ui InDimensions, ImageFormat InFormat);
+
+		/*
+		 * Constructor that copies raw image data from external array
+		 * May apply some conversions to make sure that it stores data in optimal manner(e.g. each pixel
+		 * could be stored in default types like uint8, 16, 32, 64 etc.)
+		 */
+		ImageAsset(const String& Name, Vec2ui InDimensions, ImageFormat InFormat, const uint8* InData);
+
+		friend class AssetLoader;
+
+		std::vector<uint8> Data;
+		Vec2ui Dimensions;
+		ImageFormat Format;
+	};
+}

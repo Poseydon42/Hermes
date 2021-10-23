1723ca883b0ea1cd778e1a8da455e0f541b8948f
Poseydon42 <vvmposeydon@gmail.com>
2021-10-23 23:40:48 +0300

    Core: formatted NonCopyableMovable.h
    It appeared that missing '\n' in the end of the file was causing
    troubles, but now it is fixed.

--- Hermes/Source/Core/Misc/NonCopyableMovable.h
+++ Hermes/Source/Core/Misc/NonCopyableMovable.h
@@ -2,12 +2,12 @@
 
 #include "Core/Core.h"
 
-#define MAKE_NON_COPYABLE(Name) \
-	public: \
-		Name(const Name&) = delete; \
+#define MAKE_NON_COPYABLE(Name)             \
+	public:                                  \
+		Name(const Name&) = delete;           \
 		Name& operator=(const Name&) = delete; \
 
-#define MAKE_NON_MOVABLE(Name) \
-	public: \
-		Name(Name&& Other) = delete; \
+#define MAKE_NON_MOVABLE(Name)         \
+	public:                             \
+		Name(Name&& Other) = delete;     \
 		Name& operator=(Name&&) = delete; \

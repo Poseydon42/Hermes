0b91245e12d395e5d2ed4b2bcce0b2732a12d855
Poseydon42 <vvmposeydon@gmail.com>
2021-10-27 01:23:19 +0300

    AssetSystem: Implemented MeshAsset

---
+++ Hermes/Source/AssetSystem/MeshAsset.cpp
@@ -0,0 +1,58 @@
+﻿#include "MeshAsset.h"
+
+namespace Hermes
+{
+	size_t MeshAsset::GetMemorySize() const
+	{
+		return Indices.size() * sizeof(Indices[0]) + Vertices.size() * sizeof(Vertices[0]);
+	}
+
+	bool MeshAsset::IsValid() const
+	{
+		return (Indices.size() % 3 == 0 && !Indices.empty() && !Vertices.empty());
+	}
+
+	const Vertex* MeshAsset::GetRawVertexData() const
+	{
+		return Vertices.data();
+	}
+
+	const uint32* MeshAsset::GetRawIndexData() const
+	{
+		return Indices.data();
+	}
+
+	uint32 MeshAsset::GetVertexCount() const
+	{
+		HERMES_ASSERT_LOG(Vertices.size() <= UINT32_MAX, L"Cant load meshes with more than 2^32 - 1 vertices");
+		return static_cast<uint32>(Vertices.size());
+	}
+
+	uint32 MeshAsset::GetIndexCount() const
+	{
+		HERMES_ASSERT_LOG(Indices.size() <= UINT32_MAX, L"Cant load meshes with more than 2^32 - 1 indices");
+		return static_cast<uint32>(Indices.size());
+	}
+
+	uint32 MeshAsset::GetTriangleCount() const
+	{
+		return GetIndexCount() / 3;
+	}
+
+	size_t MeshAsset::GetRequiredVertexBufferSize() const
+	{
+		return GetVertexCount() * sizeof(Vertices[0]);
+	}
+
+	size_t MeshAsset::GetRequiredIndexBufferSize() const
+	{
+		return GetIndexCount() * sizeof(Indices[0]);
+	}
+
+	MeshAsset::MeshAsset(const String& Name, std::vector<Vertex> VertexData, std::vector<uint32> IndexData)
+		: Asset(Name, AssetType::Mesh)
+		, Vertices(std::move(VertexData))
+		, Indices(std::move(IndexData))
+	{
+	}
+}
---
+++ Hermes/Source/AssetSystem/MeshAsset.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include "AssetSystem/Asset.h"
+#include "Core/Core.h"
+#include "Math/Math.h"
+
+namespace Hermes
+{
+	struct Vertex
+	{
+		Vec3 Position;
+		Vec2 TextureCoordinates;
+		Vec3 Normal;
+	};
+
+	/*
+	 * In Hermes' representation, mesh is simply an array of raw vertex and index data for object to be
+	 * drawn on the screen. It does not contain any material, hierarchy or animation information.
+	 * */
+	class HERMES_API MeshAsset : public Asset
+	{
+	public:
+		virtual size_t GetMemorySize() const override;
+
+		virtual bool IsValid() const override;
+
+		const Vertex* GetRawVertexData() const;
+
+		const uint32* GetRawIndexData() const;
+
+		uint32 GetVertexCount() const;
+
+		uint32 GetIndexCount() const;
+
+		uint32 GetTriangleCount() const;
+
+		size_t GetRequiredVertexBufferSize() const;
+
+		size_t GetRequiredIndexBufferSize() const;
+	private:
+		MeshAsset(const String& Name, std::vector<Vertex> VertexData, std::vector<uint32> IndexData);
+
+		std::vector<Vertex> Vertices;
+		std::vector<uint32> Indices;
+
+		friend class AssetLoader;
+	};
+}

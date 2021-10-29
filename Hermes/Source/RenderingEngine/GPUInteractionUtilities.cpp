0396a6333f1cd429d4beec4fd379974a6bf2cc42
Poseydon42 <vvmposeydon@gmail.com>
2021-10-30 00:01:46 +0300

    RenderingEngine: Added helper class GPUInteractionUtilities
    It is going to be responsible for default and often repeated operations
    like uploading buffers etc.

---
+++ Hermes/Source/RenderingEngine/GPUInteractionUtilities.cpp
@@ -0,0 +1,58 @@
+﻿#include "GPUInteractionUtilities.h"
+
+#include "RenderInterface/GenericRenderInterface/CommandBuffer.h"
+#include "RenderInterface/GenericRenderInterface/Fence.h"
+#include "Math/Common.h"
+#include "RenderingEngine/Renderer.h"
+
+namespace Hermes
+{
+	std::shared_ptr<RenderInterface::Buffer> GPUInteractionUtilities::StagingBuffer;
+
+	void GPUInteractionUtilities::UploadDataToGPUBuffer(const void* Data, size_t DataSize, size_t TargetOffset, RenderInterface::Buffer& Target)
+	{
+		auto& Device = Renderer::Get().GetActiveDevice();
+
+		auto& CurrentStagingBuffer = EnsureStagingBuffer();
+		auto TransferQueue = Device.GetQueue(RenderInterface::QueueType::Transfer);
+		auto TransferCommandBuffer = TransferQueue->CreateCommandBuffer(true);
+		auto TransferFinishedFence = Device.CreateFence(true);
+		HERMES_ASSERT(TargetOffset + DataSize <= Target.GetSize())
+
+		for (size_t DataOffset = 0; DataOffset < DataSize; DataOffset += CurrentStagingBuffer.GetSize())
+		{
+			size_t NumBytesToCopy = Math::Min(DataSize - DataOffset, CurrentStagingBuffer.GetSize());
+
+			auto* MappedBufferData = CurrentStagingBuffer.Map();
+			memcpy(MappedBufferData, reinterpret_cast<const uint8*>(Data) + DataOffset, NumBytesToCopy);
+			CurrentStagingBuffer.Unmap();
+
+			TransferFinishedFence->Wait(UINT64_MAX);
+			TransferFinishedFence->Reset();
+
+			TransferCommandBuffer->BeginRecording();
+			RenderInterface::BufferCopyRegion Region;
+			Region.NumBytes = NumBytesToCopy;
+			Region.SourceOffset = 0;
+			Region.DestinationOffset = DataOffset;
+			TransferCommandBuffer->CopyBuffer(CurrentStagingBuffer, Target, { Region });
+			TransferCommandBuffer->EndRecording();
+
+			TransferQueue->SubmitCommandBuffer(TransferCommandBuffer, TransferFinishedFence);
+			TransferFinishedFence->Wait(UINT64_MAX);
+		}
+	}
+
+	RenderInterface::Buffer& GPUInteractionUtilities::EnsureStagingBuffer()
+	{
+		if (StagingBuffer)
+			return *StagingBuffer;
+
+		static constexpr size_t StagingBufferSize = 8 * 1024 * 1024;
+
+		auto& Device = Renderer::Get().GetActiveDevice();
+		StagingBuffer = Device.CreateBuffer(StagingBufferSize, RenderInterface::BufferUsageType::CPUAccessible | RenderInterface::BufferUsageType::CopySource);
+		HERMES_ASSERT_LOG(StagingBuffer, L"Failed to create staging buffer with size %ull", StagingBufferSize);
+		return *StagingBuffer;
+	}
+}
---
+++ Hermes/Source/RenderingEngine/GPUInteractionUtilities.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "Core/Core.h"
+#include "RenderInterface/GenericRenderInterface/Buffer.h"
+#include "RenderInterface/GenericRenderInterface/Device.h"
+
+namespace Hermes
+{
+	class HERMES_API GPUInteractionUtilities
+	{
+	public:
+		static void UploadDataToGPUBuffer(const void* Data, size_t DataSize, size_t TargetOffset, RenderInterface::Buffer& Target);
+
+	private:
+		static RenderInterface::Buffer& EnsureStagingBuffer();
+
+		static std::shared_ptr<RenderInterface::Buffer> StagingBuffer;
+	};
+}

d8fdce80f9f8f50b0cc680e716808ebc77e86a7d
Poseydon42 <vvmposeydon@gmail.com>
2021-10-30 00:02:57 +0300

    RenderingSystem: Introduced a MeshBuffer struct that is a representation of a MeshAsset in the rendering infrastructure

---
+++ Hermes/Source/RenderingEngine/MeshBuffer.cpp
@@ -0,0 +1,63 @@
+﻿#include "MeshBuffer.h"
+
+#include "RenderingEngine/GPUInteractionUtilities.h"
+#include "RenderingEngine/Renderer.h"
+
+namespace Hermes
+{
+	MeshBuffer::MeshBuffer(std::weak_ptr<MeshAsset> InAsset)
+		: DataUploadFinished(false)
+		, IndexCount(0)
+		, Asset(std::move(InAsset))
+	{
+		auto LockedAsset = Asset.lock();
+		if (!LockedAsset)
+			return;
+
+		auto& Device = Renderer::Get().GetActiveDevice();
+
+		VertexBuffer = Device.CreateBuffer(LockedAsset->GetRequiredVertexBufferSize(), RenderInterface::BufferUsageType::VertexBuffer | RenderInterface::BufferUsageType::CopyDestination);
+		IndexBuffer = Device.CreateBuffer(LockedAsset->GetRequiredIndexBufferSize(), RenderInterface::BufferUsageType::IndexBuffer | RenderInterface::BufferUsageType::CopyDestination);
+
+		GPUInteractionUtilities::UploadDataToGPUBuffer(LockedAsset->GetRawVertexData(), LockedAsset->GetRequiredVertexBufferSize(), 0, *VertexBuffer);
+		GPUInteractionUtilities::UploadDataToGPUBuffer(LockedAsset->GetRawIndexData(), LockedAsset->GetRequiredIndexBufferSize(), 0, *IndexBuffer);
+
+		IndexCount = LockedAsset->GetIndexCount();
+		DataUploadFinished = true;
+	}
+
+	std::shared_ptr<MeshBuffer> MeshBuffer::CreateFromAsset(std::weak_ptr<MeshAsset> InAsset)
+	{
+		if (InAsset.expired())
+			return nullptr;
+		return std::shared_ptr<MeshBuffer>(new MeshBuffer(std::move(InAsset)));
+	}
+
+	const RenderInterface::Buffer& MeshBuffer::GetVertexBuffer() const
+	{
+		return *VertexBuffer;
+	}
+
+	const RenderInterface::Buffer& MeshBuffer::GetIndexBuffer() const
+	{
+		return *IndexBuffer;
+	}
+
+	bool MeshBuffer::IsReady() const
+	{
+		return VertexBuffer && IndexBuffer && DataUploadFinished;
+	}
+
+	MeshBuffer::MeshDrawInformation MeshBuffer::GetDrawInformation() const
+	{
+		HERMES_ASSERT(IsReady());
+
+		MeshDrawInformation Result;
+		Result.IndexCount = IndexCount;
+		Result.IndexOffset = 0;
+		Result.VertexOffset = 0;
+
+		return Result;
+	}
+
+}
---
+++ Hermes/Source/RenderingEngine/MeshBuffer.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <memory>
+
+#include "AssetSystem/MeshAsset.h"
+#include "Core/Core.h"
+#include "RenderInterface/GenericRenderInterface/Buffer.h"
+#include "RenderInterface/GenericRenderInterface/Device.h"
+
+namespace Hermes
+{
+	class HERMES_API MeshBuffer
+	{
+	public:
+		static std::shared_ptr<MeshBuffer> CreateFromAsset(std::weak_ptr<MeshAsset> InAsset);
+
+		const RenderInterface::Buffer& GetVertexBuffer() const;
+		const RenderInterface::Buffer& GetIndexBuffer() const;
+
+		bool IsReady() const;
+
+		struct MeshDrawInformation
+		{
+			uint32 IndexCount;
+			uint32 IndexOffset;
+			uint32 VertexOffset;
+		};
+
+		MeshDrawInformation GetDrawInformation() const;
+
+	private:
+		explicit MeshBuffer(std::weak_ptr<MeshAsset> InAsset);
+
+		bool DataUploadFinished;
+		uint32 IndexCount;
+		std::weak_ptr<MeshAsset> Asset;
+		std::shared_ptr<RenderInterface::Buffer> VertexBuffer, IndexBuffer;
+	};
+}

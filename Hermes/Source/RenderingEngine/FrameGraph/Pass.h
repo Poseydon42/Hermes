f070b0bff8af86c9fb7a645f129b4f1c6323d2e4
Poseydon42 <vvmposeydon@gmail.com>
2022-02-07 17:16:28 +0200

    RenderingEngine: added declarations of some structs related to frame graph system

---
+++ Hermes/Source/RenderingEngine/FrameGraph/Pass.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "Core/Core.h"
+#include "Core/Delegate/Delegate.h"
+
+namespace Hermes
+{
+	namespace RenderInterface
+	{
+		class CommandBuffer;
+	}
+
+	class Scene;
+	struct Source;
+	struct Drain;
+
+	struct PassDesc
+	{
+		using RenderPassCallbackType = TDelegate<void, RenderInterface::CommandBuffer&, const Scene&>;
+		
+		std::vector<Drain> Drains;
+		std::vector<Source> Sources;
+		RenderPassCallbackType Callback;
+	};
+}
---
+++ Hermes/Source/RenderingEngine/FrameGraph/Resource.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+
+#include "Core/Core.h"
+#include "Math/Math.h"
+#include "RenderInterface/GenericRenderInterface/CommonTypes.h"
+#include "RenderInterface/GenericRenderInterface/RenderPass.h"
+
+namespace Hermes
+{
+	struct ResourceDesc
+	{
+		RenderInterface::DataFormat Format;
+		Vec2ui Dimensions;
+		uint32 MipLevels;
+	};
+
+	// TODO : input attachment binding support
+	enum class BindingMode
+	{
+		ColorAttachment,
+		DepthStencilAttachment
+	};
+
+	struct Drain
+	{
+		String Name;
+		RenderInterface::DataFormat Format;
+		RenderInterface::AttachmentLoadOp LoadOp;
+		RenderInterface::AttachmentLoadOp StencilLoadOp;
+		float ClearColor[4];
+		RenderInterface::ImageLayout Layout;
+		BindingMode Binding;
+	};
+
+	struct Source
+	{
+		String Name;
+		RenderInterface::DataFormat Format;
+	};
+}

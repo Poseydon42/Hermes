31158dd1afa8f14cf9870ec9d940346a82e3af92
Poseydon42 <vvmposeydon@gmail.com>
2022-06-30 11:01:06 +0100

    Core: added error message when trying to compile with something different than MSVC

--- Hermes/Source/Core/Compiler/CompilerMSVC.h
+++ Hermes/Source/Core/Compiler/CompilerMSVC.h
@@ -1,25 +1,28 @@
 #pragma once
 
-// TODO : premake does not seem to add _MSVC_VER to preprocessor definitions,
-// maybe we should do it in a different way if we're ever going to compile
-// using other compiler than MSVC on Windows
 #ifdef HERMES_PLATFORM_WINDOWS
 
-#define API_EXPORT _declspec(dllexport)
-#define API_IMPORT _declspec(dllimport)
+#	ifdef _MSC_VER
 
-#define DEBUG_BREAK() __debugbreak();
+#		define API_EXPORT _declspec(dllexport)
+#		define API_IMPORT _declspec(dllimport)
 
-#define SUPPRESS_ALL_WARNINGS_BEGIN \
-	__pragma(warning(push, 0))
+#		define DEBUG_BREAK() __debugbreak();
 
-#define SUPPRESS_ALL_WARNINGS_END \
-	__pragma(warning(pop))
+#		define SUPPRESS_ALL_WARNINGS_BEGIN \
+			__pragma(warning(push, 0))
 
-#define PACKED_STRUCT_BEGIN \
-	__pragma(pack(push, 1))
+#		define SUPPRESS_ALL_WARNINGS_END \
+			__pragma(warning(pop))
 
-#define PACKED_STRUCT_END \
-	__pragma(pack(pop))
+#		define PACKED_STRUCT_BEGIN \
+			__pragma(pack(push, 1))
+
+#		define PACKED_STRUCT_END \
+			__pragma(pack(pop))
+
+#	else
+#		error "Hermes does not support compilers other than MSVC yet"
+#	endif
 
 #endif

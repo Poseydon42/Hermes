45a84069f99c2667a052db7c9f8fe3e75ed2d914
Poseydon42 <vvmposeydon@gmail.com>
2022-07-04 12:02:01 +0100

    Shaders: moved uniform cube vertices array to separate include glsl file

--- Hermes/Files/Shaders/build_all.bat
+++ Hermes/Files/Shaders/build_all.bat
@@ -1,3 +1,3 @@
 @echo off
 if not exist Bin\ mkdir Bin
-for %%v in (*.glsl) do "%VULKAN_SDK%/Bin/glslc.exe" %%v -o Bin/%%v.spv --target-env=vulkan1.1
+for %%v in (*.glsl) do "%VULKAN_SDK%/Bin/glslc.exe" %%v -o Bin/%%v.spv -I Common --target-env=vulkan1.1
--- Hermes/Files/Shaders/load_equirectangular_vert.glsl
+++ Hermes/Files/Shaders/load_equirectangular_vert.glsl
@@ -1,6 +1,8 @@
 #version 450
 #pragma shader_stage(vertex)
 
+#include "uniform_cube.glsl"
+
 layout(location = 0) out vec3 o_Pos;
 
 layout(push_constant, row_major) uniform PushConstants
@@ -8,54 +10,10 @@ layout(push_constant, row_major) uniform PushConstants
     mat4 ViewProjection;
 } i_PushConstants;
 
-vec3 Vertices[36] = {
-    // back face
-    { -1.0f, -1.0f, -1.0f }, // bottom-left
-    {  1.0f,  1.0f, -1.0f }, // top-right
-    {  1.0f, -1.0f, -1.0f }, // bottom-right         
-    {  1.0f,  1.0f, -1.0f }, // top-right
-    { -1.0f, -1.0f, -1.0f }, // bottom-left
-    { -1.0f,  1.0f, -1.0f }, // top-left
-    // front face
-    { -1.0f, -1.0f,  1.0f }, // bottom-left
-    {  1.0f, -1.0f,  1.0f }, // bottom-right
-    {  1.0f,  1.0f,  1.0f }, // top-right
-    {  1.0f,  1.0f,  1.0f }, // top-right
-    { -1.0f,  1.0f,  1.0f }, // top-left
-    { -1.0f, -1.0f,  1.0f }, // bottom-left
-    // left face
-    { -1.0f,  1.0f,  1.0f }, // top-right
-    { -1.0f,  1.0f, -1.0f }, // top-left
-    { -1.0f, -1.0f, -1.0f }, // bottom-left
-    { -1.0f, -1.0f, -1.0f }, // bottom-left
-    { -1.0f, -1.0f,  1.0f }, // bottom-right
-    { -1.0f,  1.0f,  1.0f }, // top-right
-    // right face
-    {  1.0f,  1.0f,  1.0f }, // top-left
-    {  1.0f, -1.0f, -1.0f }, // bottom-right
-    {  1.0f,  1.0f, -1.0f }, // top-right         
-    {  1.0f, -1.0f, -1.0f }, // bottom-right
-    {  1.0f,  1.0f,  1.0f }, // top-left
-    {  1.0f, -1.0f,  1.0f }, // bottom-left     
-    // bottom face
-    { -1.0f, -1.0f, -1.0f }, // top-right
-    {  1.0f, -1.0f, -1.0f }, // top-left
-    {  1.0f, -1.0f,  1.0f }, // bottom-left
-    {  1.0f, -1.0f,  1.0f }, // bottom-left
-    { -1.0f, -1.0f,  1.0f }, // bottom-right
-    { -1.0f, -1.0f, -1.0f }, // top-right
-    // top face
-    { -1.0f,  1.0f, -1.0f }, // top-left
-    {  1.0f,  1.0f , 1.0f }, // bottom-right
-    {  1.0f,  1.0f, -1.0f }, // top-right     
-    {  1.0f,  1.0f,  1.0f }, // bottom-right
-    { -1.0f,  1.0f, -1.0f }, // top-left
-    { -1.0f,  1.0f,  1.0f }  // bottom-left    
-};
 
 void main()
 {
-    vec3 LocalPos = Vertices[gl_VertexIndex];
+    vec3 LocalPos = UniformCubeVertices[gl_VertexIndex];
     
     vec4 VertexPos = vec4(LocalPos, 1.0);
     VertexPos = i_PushConstants.ViewProjection * VertexPos;

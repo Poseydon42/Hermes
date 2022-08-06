221974ec2a44d4f26989daf200eb5828a7a06032
Poseydon42 <vvmposeydon@gmail.com>
2022-08-06 17:13:11 +0100

    Meta: enable all warnings and treat them like errors. The project is not going to build for the next few commits

--- Meta/CMakeCommon.cmake
+++ Meta/CMakeCommon.cmake
@@ -9,6 +9,8 @@ if(${WIN32})
     add_compile_definitions(HERMES_PLATFORM_WINDOWS)
     add_compile_definitions(UNICODE _UNICODE)
     add_compile_options("/MP")
+    add_compile_options(/W4 /WX) # Enable all warnings and treat them as errors
+    add_compile_options(/wd4251) # And disable warning C4251(... needs to have dll-interface to be used by clients of class ...)
 else()
     message(FATAL_ERROR "Platforms other than Windows are not supported yet")
 endif()

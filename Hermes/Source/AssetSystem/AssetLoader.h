2da580a09ff67a19d5099b7b569ec6bcebf567d8
Poseydon42 <vvmposeydon@gmail.com>
2022-08-13 14:54:08 +0100

    AssetSystem: move asset header struct out of AssetLoader private members

--- Hermes/Source/AssetSystem/AssetLoader.h
+++ Hermes/Source/AssetSystem/AssetLoader.h
@@ -10,19 +10,20 @@ namespace Hermes
 {
 	class IPlatformFile;
 
+	PACKED_STRUCT_BEGIN
+	struct AssetHeader
+	{
+		AssetType Type;
+		// TODO : version, dependencies etc.
+	};
+	PACKED_STRUCT_END
+
 	class HERMES_API AssetLoader
 	{
 	public:
 		static std::shared_ptr<Asset> Load(const String& Name);
 
 	private:
-		PACKED_STRUCT_BEGIN
-		struct AssetHeader
-		{
-			AssetType Type;
-			// TODO : version, dependencies etc.
-		};
-		PACKED_STRUCT_END
 
 		static std::shared_ptr<Asset> LoadImage(IPlatformFile& File, const AssetHeader& Header, const String& Name);
 

c32763cd6ec583fccbbca2deb28a5948813158ed
Poseydon42 <vvmposeydon@gmail.com>
2022-08-24 14:16:56 +0100

    RenderingEngine: compute the irradiance cubemap on engine load instead of loading it from a file

---
+++ Hermes/Files/Shaders/irradiance_convolution.glsl
@@ -0,0 +1,46 @@
+#version 450
+#pragma shader_stage(fragment)
+
+layout(set = 0, binding = 0) uniform samplerCube u_Envmap;
+
+layout(location = 0) in vec3 i_Position;
+
+layout(location = 0) out vec4 o_Color;
+
+const float Pi = 3.14159265359;
+
+/* https://learnopengl.com/PBR/IBL/Diffuse-irradiance */
+void main()
+{
+    vec3 Normal = normalize(i_Position);
+
+    vec3 Result = vec3(0.0);
+
+    vec3 Up = vec3(0.0, 1.0, 0.0);
+    if (abs(Normal.y) > 0.999)
+    {
+        Up = vec3(0.0, 0.0, 1.0);
+    }
+    vec3 Right = normalize(cross(Up, Normal));
+    Up = normalize(cross(Normal, Right));
+
+    float SampleDelta = 0.025;
+    float SampleCount = 0.0;
+    for (float Phi = 0.0; Phi < 2.0 * Pi; Phi += SampleDelta)
+    {
+        for (float Theta = 0.0; Theta < 0.5 * Pi; Theta += SampleDelta)
+        {
+            // Spherical to cartesian in tangent space
+            vec3 TangentSampleDirection = vec3(sin(Theta) * cos(Phi), sin(Theta) * sin(Phi), cos(Theta));
+            // Tangent sample direction to world sample direction
+            vec3 WorldSampleDirection = TangentSampleDirection.x * Right + TangentSampleDirection.y * Up + TangentSampleDirection.z * Normal;
+
+            Result += texture(u_Envmap, WorldSampleDirection).rgb * cos(Theta) * sin(Theta);
+            SampleCount += 1.0;
+        }
+    }
+
+    Result /= SampleCount;
+
+    o_Color = vec4(Result, 1.0);
+}
--- Hermes/Source/RenderingEngine/Scene/Scene.cpp
+++ Hermes/Source/RenderingEngine/Scene/Scene.cpp
@@ -3,9 +3,189 @@
 #include "AssetSystem/AssetLoader.h"
 #include "AssetSystem/ImageAsset.h"
 #include "Logging/Logger.h"
+#include "RenderingEngine/DescriptorAllocator.h"
+#include "RenderingEngine/Renderer.h"
+#include "RenderInterface/GenericRenderInterface/Device.h"
+#include "RenderInterface/GenericRenderInterface/Fence.h"
+#include "RenderInterface/GenericRenderInterface/Queue.h"
 
 namespace Hermes
 {
+	static std::unique_ptr<CubemapTexture> ComputeIrradianceCubemap(const CubemapTexture& Source)
+	{
+		const Vec2ui Dimensions = { 16 };
+
+		static constexpr std::array CubemapSides =
+		{
+			RenderInterface::CubemapSide::PositiveX, RenderInterface::CubemapSide::NegativeX,
+			RenderInterface::CubemapSide::PositiveY, RenderInterface::CubemapSide::NegativeY,
+			RenderInterface::CubemapSide::PositiveZ, RenderInterface::CubemapSide::NegativeZ
+		};
+
+		struct PushConstants
+		{
+			Mat4 MVP;
+		};
+
+		std::unique_ptr<CubemapTexture> Result = CubemapTexture::CreateEmpty(Dimensions,
+		                                                                     RenderInterface::DataFormat::R32G32B32A32SignedFloat,
+		                                                                     RenderInterface::ImageUsageType::ColorAttachment
+		                                                                     | RenderInterface::ImageUsageType::Sampled,
+		                                                                     1);
+
+		auto& Device = Renderer::Get().GetActiveDevice();
+		auto& Queue = Device.GetQueue(RenderInterface::QueueType::Render);
+
+		auto CommandBuffer = Queue.CreateCommandBuffer(true);
+
+		auto VertexShader = Device.CreateShader(L"Shaders/Bin/render_uniform_cube.glsl.spv",
+		                                        RenderInterface::ShaderType::VertexShader);
+		auto FragmentShader = Device.CreateShader(L"Shaders/Bin/irradiance_convolution.glsl.spv",
+		                                          RenderInterface::ShaderType::FragmentShader);
+
+		RenderInterface::SamplerDescription SamplerDesc = {};
+		SamplerDesc.AddressingModeU = RenderInterface::AddressingMode::Repeat;
+		SamplerDesc.AddressingModeV = RenderInterface::AddressingMode::Repeat;
+		SamplerDesc.MinificationFilteringMode = RenderInterface::FilteringMode::Linear;
+		SamplerDesc.MagnificationFilteringMode = RenderInterface::FilteringMode::Linear;
+		SamplerDesc.CoordinateSystem = RenderInterface::CoordinateSystem::Normalized;
+		SamplerDesc.MipMode = RenderInterface::MipmappingMode::Linear;
+		SamplerDesc.MinMipLevel = 0.0f;
+		SamplerDesc.MaxMipLevel = static_cast<float>(Source.GetMipLevelsCount());
+		SamplerDesc.MipBias = 0.0f;
+		auto Sampler = Device.CreateSampler(SamplerDesc);
+
+		RenderInterface::DescriptorBinding SourceTextureBinding = {};
+		SourceTextureBinding.Index = 0;
+		SourceTextureBinding.DescriptorCount = 1;
+		SourceTextureBinding.Shader = RenderInterface::ShaderType::FragmentShader;
+		SourceTextureBinding.Type = RenderInterface::DescriptorType::CombinedSampler;
+		auto DescriptorLayout = Device.CreateDescriptorSetLayout({ SourceTextureBinding });
+		auto DescriptorSet = Renderer::Get().GetDescriptorAllocator().Allocate(*DescriptorLayout);
+		DescriptorSet->UpdateWithImageAndSampler(0, 0, Source.GetDefaultView(), *Sampler,
+		                                         RenderInterface::ImageLayout::ShaderReadOnlyOptimal);
+
+		RenderInterface::RenderPassAttachment OutputAttachmentDesc = {};
+		OutputAttachmentDesc.Type = RenderInterface::AttachmentType::Color;
+		OutputAttachmentDesc.LoadOp = RenderInterface::AttachmentLoadOp::Clear;
+		OutputAttachmentDesc.StoreOp = RenderInterface::AttachmentStoreOp::Store;
+		OutputAttachmentDesc.StencilLoadOp = RenderInterface::AttachmentLoadOp::Undefined;
+		OutputAttachmentDesc.StencilStoreOp = RenderInterface::AttachmentStoreOp::Undefined;
+		OutputAttachmentDesc.Format = Result->GetDataFormat();
+		OutputAttachmentDesc.LayoutAtStart = RenderInterface::ImageLayout::ColorAttachmentOptimal;
+		OutputAttachmentDesc.LayoutAtEnd = RenderInterface::ImageLayout::ShaderReadOnlyOptimal;
+		auto RenderPass = Device.CreateRenderPass({ OutputAttachmentDesc });
+
+		RenderInterface::PipelineDescription PipelineDesc = {};
+		PipelineDesc.PushConstants = { { RenderInterface::ShaderType::VertexShader, 0, sizeof(PushConstants) } };
+		PipelineDesc.ShaderStages = { VertexShader.get(), FragmentShader.get() };
+		PipelineDesc.DescriptorLayouts = { DescriptorLayout.get() };
+		PipelineDesc.InputAssembler.Topology = RenderInterface::TopologyType::TriangleList;
+		PipelineDesc.Viewport.Origin = { 0, 0 };
+		PipelineDesc.Viewport.Dimensions = Dimensions;
+		PipelineDesc.Rasterizer.Direction = RenderInterface::FaceDirection::CounterClockwise;
+		PipelineDesc.Rasterizer.Cull = RenderInterface::CullMode::Back;
+		PipelineDesc.Rasterizer.Fill = RenderInterface::FillMode::Fill;
+		PipelineDesc.DepthStencilStage.IsDepthTestEnabled = false;
+		PipelineDesc.DepthStencilStage.IsDepthWriteEnabled = false;
+		auto Pipeline = Device.CreatePipeline(*RenderPass, PipelineDesc);
+
+		// NOTE : we must not destroy the image view that is bound to the render target, so we need to store
+		//        an owning pointer to it as well
+		std::vector<std::pair<std::unique_ptr<RenderInterface::ImageView>, std::unique_ptr<
+			                      RenderInterface::RenderTarget>>> RenderTargets;
+		RenderInterface::ImageViewDescription ViewDesc = {};
+		ViewDesc.BaseMipLevel = 0;
+		ViewDesc.MipLevelCount = 1;
+		ViewDesc.Aspects = RenderInterface::ImageAspect::Color;
+		for (auto CubemapSide : CubemapSides)
+		{
+			auto View = Result->GetRawImage().CreateCubemapImageView(ViewDesc, CubemapSide);
+			auto RenderTarget = Device.CreateRenderTarget(*RenderPass, { View.get() }, Dimensions);
+			RenderTargets.emplace_back(std::move(View), std::move(RenderTarget));
+		}
+
+		/*
+		 * Steps to take:
+		 *   1) Transfer layout of the resulting cubemap from undefined (which it has after it was created) to
+		 *      color attachment optimal
+		 *	 2) Bind pipeline and descriptor set
+		 *	 3) Repeat 6 times:
+		 *	    a) Begin render pass
+		 *	    b) Upload push constants with the MVP matrix for given cubemap face
+		 *		c) Issue draw call
+		 *		d) End render pass
+		 *	 4) Transfer layout to the cubemap from color attachment optimal to shader readonly optimal (to be used
+		 *	    as a texture). This is done by the implicit render pass attachment layout transition, so we don't
+		 *	    need to insert any more barriers
+		 */
+		CommandBuffer->BeginRecording();
+		RenderInterface::ImageMemoryBarrier Barrier = {};
+		Barrier.OldLayout = RenderInterface::ImageLayout::Undefined;
+		Barrier.NewLayout = RenderInterface::ImageLayout::ColorAttachmentOptimal;
+		Barrier.OperationsThatHaveToEndBefore = RenderInterface::AccessType::None;
+		Barrier.OperationsThatCanStartAfter = RenderInterface::AccessType::None;
+		Barrier.BaseMipLevel = 0;
+		Barrier.MipLevelCount = Result->GetMipLevelsCount();
+		Barrier.Side = RenderInterface::CubemapSide::All;
+		CommandBuffer->InsertImageMemoryBarrier(Result->GetRawImage(), Barrier,
+		                                        RenderInterface::PipelineStage::BottomOfPipe,
+		                                        RenderInterface::PipelineStage::TopOfPipe);
+		CommandBuffer->BindPipeline(*Pipeline);
+		CommandBuffer->BindDescriptorSet(*DescriptorSet, *Pipeline, 0);
+
+		for (size_t SideIndex = 0; SideIndex < CubemapSides.size(); SideIndex++)
+		{
+			auto Side = CubemapSides[SideIndex];
+
+			PushConstants PushConstantsData = {};
+			auto Model = Mat4::Identity();
+			Mat4 View;
+			switch (Side)
+			{
+			case RenderInterface::CubemapSide::PositiveX:
+				View = Mat4::LookAt(Vec3 { 0.0f, 0.0f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f });
+				break;
+			case RenderInterface::CubemapSide::NegativeX:
+				View = Mat4::LookAt(Vec3 { 0.0f, 0.0f, 0.0f }, { -1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f });
+				break;
+			case RenderInterface::CubemapSide::PositiveY:
+				View = Mat4::LookAt(Vec3 { 0.0f, 0.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f });
+				break;
+			case RenderInterface::CubemapSide::NegativeY:
+				View = Mat4::LookAt(Vec3 { 0.0f, 0.0f, 0.0f }, { 0.0f, -1.0f, 0.0f }, { 0.0f, 0.0f, 1.0f });
+				break;
+			case RenderInterface::CubemapSide::PositiveZ:
+				View = Mat4::LookAt(Vec3 { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f, 0.0f });
+				break;
+			case RenderInterface::CubemapSide::NegativeZ:
+				View = Mat4::LookAt(Vec3 { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, -1.0f }, { 0.0f, 1.0f, 0.0f });
+				break;
+			default:
+				HERMES_ASSERT(false);
+				break;
+			}
+			auto Projection = Mat4::Perspective(Math::HalfPi, 1.0f, 0.1f, 100.0f);
+			PushConstantsData.MVP = Projection * View * Model;
+
+			CommandBuffer->BeginRenderPass(*RenderPass, *RenderTargets[SideIndex].second, { { 0.0f, 0.0f, 0.0f, 1.0f } });
+			CommandBuffer->UploadPushConstants(*Pipeline, RenderInterface::ShaderType::VertexShader, &PushConstantsData,
+			                                   sizeof(PushConstantsData), 0);
+			// Draw 36 vertices for 6 cube faces that are defined in the vertex shader
+			CommandBuffer->Draw(36, 1, 0, 0);
+
+			CommandBuffer->EndRenderPass();
+		}
+
+		CommandBuffer->EndRecording();
+
+		auto Fence = Device.CreateFence();
+		Queue.SubmitCommandBuffer(*CommandBuffer, Fence.get());
+		Fence->Wait(UINT64_MAX);
+
+		return Result;
+	}
+
 	Scene::Scene()
 	{
 		auto LoadCubemap = [](const String& Name)
@@ -19,7 +199,7 @@ namespace Hermes
 		};
 
 		ReflectionEnvmap = LoadCubemap(L"Textures/envmap");
-		IrradianceEnvmap = LoadCubemap(L"Textures/diffuse_envmap");
+		IrradianceEnvmap = ComputeIrradianceCubemap(*ReflectionEnvmap);
 	}
 
 	void Scene::AddMesh(MeshProxy Proxy)

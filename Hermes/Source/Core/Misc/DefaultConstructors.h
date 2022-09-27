52cec87e368b4a2d9a154234e07d4e9c4af32866
Poseydon42 <vvmposeydon@gmail.com>
2022-09-27 10:22:41 +0100

    Core: added a macro to easily declare all necessary constructors and destructor for an interface

--- Hermes/Source/Core/Misc/DefaultConstructors.h
+++ Hermes/Source/Core/Misc/DefaultConstructors.h
@@ -21,3 +21,10 @@ public:\
 public:\
 	Name(Name&&) = default; \
 	Name& operator=(Name&&) = default;
+
+#define ADD_INTERFACE_CONSTRUCTORS_AND_DESTRUCTOR(Name) \
+public: \
+	ADD_DEFAULT_CONSTRUCTOR(Name) \
+	ADD_DEFAULT_VIRTUAL_DESTRUCTOR(Name) \
+	ADD_DEFAULT_COPY_CONSTRUCTOR(Name) \
+	ADD_DEFAULT_MOVE_CONSTRUCTOR(Name)

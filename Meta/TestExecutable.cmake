80e6e8eda9569fdd87f761b5ac625907fd5bf0dc
Poseydon42 <vvmposeydon@gmail.com>
2023-02-15 21:23:48 +0000

    Meta: set working directory for each test executable to the location of its sources

--- Meta/TestExecutable.cmake
+++ Meta/TestExecutable.cmake
@@ -14,5 +14,8 @@ function(add_test_executable test_name test_sources dependencies)
 
     source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${sources})
 
-    gtest_discover_tests(${test_name})
+    gtest_discover_tests(
+        ${test_name}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
 endfunction()

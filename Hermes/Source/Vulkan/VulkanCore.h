5866eccb9b9e20bf7927a71e901b800a5d8f531f
Poseydon42 <vvmposeydon@gmail.com>
2023-04-09 12:04:19 +0100

    Vulkan: increase required Vulkan version to 1.3

--- Hermes/Source/Vulkan/VulkanCore.h
+++ Hermes/Source/Vulkan/VulkanCore.h
@@ -14,7 +14,7 @@ SUPPRESS_ALL_WARNINGS_END
 namespace Hermes::Vulkan
 {
 	extern VkAllocationCallbacks* GVulkanAllocator;
-	constexpr uint32 GVulkanVersion = VK_API_VERSION_1_2;
+	constexpr uint32 GVulkanVersion = VK_API_VERSION_1_3;
 
 	inline String VkResultToString(VkResult Result)
 	{

e7cd250d846dfb6f03120a62e8cfda7a521d43f5
Poseydon42 <vvmposeydon@gmail.com>
2023-04-10 13:27:13 +0100

    RenderingEngine: render UI with alpha blending enabled

--- Hermes/Source/RenderingEngine/Passes/UIPass.cpp
+++ Hermes/Source/RenderingEngine/Passes/UIPass.cpp
@@ -103,7 +103,7 @@ namespace Hermes
 		const auto& FragmentShader = ShaderCache.GetShader("/Shaders/Bin/fs_ui_frag.glsl.spv", VK_SHADER_STAGE_FRAGMENT_BIT);
 
 		Vulkan::PipelineDescription Desc = {
-			.PushConstants = { VkPushConstantRange { .stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, .offset = 0, .size = sizeof(UIShaderPushConstants)}},
+			.PushConstants = { VkPushConstantRange {.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, .offset = 0, .size = sizeof(UIShaderPushConstants)}},
 			.DescriptorSetLayouts = { DescriptorSetLayout.get() },
 			.ShaderStages = { &VertexShader, &FragmentShader },
 			.VertexInputBindings = {},
@@ -112,6 +112,18 @@ namespace Hermes
 			.PolygonMode = VK_POLYGON_MODE_FILL,
 			.CullMode = VK_CULL_MODE_NONE,
 			.FaceDirection = VK_FRONT_FACE_CLOCKWISE,
+			.AttachmentColorBlending = {
+				VkPipelineColorBlendAttachmentState {
+					.blendEnable = true,
+					.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+					.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+					.colorBlendOp = VK_BLEND_OP_ADD,
+					.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
+					.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+					.alphaBlendOp = VK_BLEND_OP_ADD,
+					.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT
+				}
+			},
 			.IsDepthTestEnabled = false,
 			.IsDepthWriteEnabled = false,
 			.DepthCompareOperator = VK_COMPARE_OP_NEVER,

0c1eb672aca17101634729002ab1c255f5521fd1
Poseydon42 <vvmposeydon@gmail.com>
2023-04-13 09:02:37 +0100

    UIEngine: implement viewport widget

---
+++ Hermes/Source/UIEngine/Widgets/Containers/ViewportContainerWidget.cpp
@@ -0,0 +1,66 @@
+#include "ViewportContainerWidget.h"
+
+namespace Hermes::UI
+{
+	std::shared_ptr<ViewportContainerWidget> ViewportContainerWidget::Create(std::shared_ptr<Widget> InParentWidget)
+	{
+		return std::shared_ptr<ViewportContainerWidget>(new ViewportContainerWidget(std::move(InParentWidget)));
+	}
+
+	ViewportContainerWidget::ViewportContainerWidget(std::shared_ptr<Widget> InParentWidget)
+		: ContainerWidget(std::move(InParentWidget))
+	{
+	}
+
+	Vec2 ViewportContainerWidget::ComputeMinimumDimensions() const
+	{
+		Vec2 MinDimensions = {};
+		ForEachChild([&](const Widget& Child)
+		{
+			auto ChildMinDimensions = Child.ComputeMinimumDimensions();
+			MinDimensions.X = Math::Max(MinDimensions.X, ChildMinDimensions.X);
+			MinDimensions.Y = Math::Max(MinDimensions.Y, ChildMinDimensions.Y);
+		});
+		return MinDimensions;
+	}
+
+	void ViewportContainerWidget::Draw(DrawingContext& Context, Rect2D AvailableRect) const
+	{
+		Context.SetSceneViewport(AvailableRect);
+
+		ForEachChild([&](const Widget& Child)
+		{
+			auto AbsoluteMarginLeft = GetAbsoluteMarginValue(Child.GetMargins().Left, AvailableRect.Width());
+			auto AbsoluteMarginRight = GetAbsoluteMarginValue(Child.GetMargins().Right, AvailableRect.Width());
+			auto AbsoluteMarginTop = GetAbsoluteMarginValue(Child.GetMargins().Top, AvailableRect.Height());
+			auto AbsoluteMarginBottom = GetAbsoluteMarginValue(Child.GetMargins().Bottom, AvailableRect.Height());
+
+			auto ChildMinDimensions = Child.ComputeMinimumDimensions();
+
+			float TotalUnscaledWidth = AbsoluteMarginLeft + ChildMinDimensions.X + AbsoluteMarginRight;
+			if (TotalUnscaledWidth > AvailableRect.Width())
+			{
+				float ScalingFactor = (AvailableRect.Width() - ChildMinDimensions.X) / (TotalUnscaledWidth - ChildMinDimensions.X);
+				AbsoluteMarginLeft *= ScalingFactor;
+				AbsoluteMarginRight *= ScalingFactor;
+			}
+
+			float TotalUnscaledHeight = AbsoluteMarginTop + ChildMinDimensions.Y + AbsoluteMarginBottom;
+			if (TotalUnscaledHeight > AvailableRect.Height())
+			{
+				float ScalingFactor = (AvailableRect.Height() - ChildMinDimensions.Y) / (TotalUnscaledHeight - ChildMinDimensions.Y);
+				AbsoluteMarginTop *= ScalingFactor;
+				AbsoluteMarginBottom *= ScalingFactor;
+			}
+
+			Rect2D ChildRect = {
+				.Min = { AbsoluteMarginLeft, AbsoluteMarginTop },
+				.Max = { AvailableRect.Width() - AbsoluteMarginRight, AvailableRect.Height() - AbsoluteMarginBottom }
+			};
+			HERMES_ASSERT(ChildRect.Width() >= ChildMinDimensions.X);
+			HERMES_ASSERT(ChildRect.Height() >= ChildMinDimensions.Y);
+
+			Child.Draw(Context, ChildRect);
+		});
+	}
+}
---
+++ Hermes/Source/UIEngine/Widgets/Containers/ViewportContainerWidget.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Core/Core.h"
+#include "UIEngine/Widgets/Containers/ContainerWidget.h"
+
+namespace Hermes::UI
+{
+	/*
+	 * Represents a viewport that the scene is renderer to. It can also serve as a container for other widgets. Those
+	 * widgets will be drawn inside the viewport. There is no strict layout process applied to those widgets - they
+	 * are given as much space as the viewport and their margins permit, meaning that some of them might be overlapping.
+	 *
+	 * There are a lot of things TODO, some of them depend on the renderer as well:
+	 *  - allow multiple viewports
+	 *	- somehow pass the information about camera to the viewport
+	 *	- allow multiple different scenes to be rendered
+	 */
+	class HERMES_API ViewportContainerWidget : public ContainerWidget
+	{
+	public:
+		virtual ~ViewportContainerWidget() override = default;
+
+		static std::shared_ptr<ViewportContainerWidget> Create(std::shared_ptr<Widget> InParentWidget);
+
+	protected:
+		explicit ViewportContainerWidget(std::shared_ptr<Widget> InParentWidget);
+		
+		virtual Vec2 ComputeMinimumDimensions() const override;
+
+		virtual void Draw(DrawingContext& Context, Rect2D AvailableRect) const override;
+	};
+}

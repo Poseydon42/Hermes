7cace905fc41fc8aeb18a91758340fa5e515799a
Poseydon42 <vvmposeydon@gmail.com>
2023-05-09 19:23:38 +0100

    Meta: renamed add_hermes_sublib() into add_engine_sublib()

--- Meta/HermesSublib.cmake
+++ Meta/HermesSublib.cmake
@@ -1,21 +1,27 @@
 include(CommonDefinitions)
 
 function(add_hermes_sublib name sources)
-    project(${name} C CXX)
+    project(${name} CXX)
 
     add_library(${name} STATIC ${sources})
     add_common_definitions(${name})
 
     target_compile_definitions(${name} PRIVATE $<$<CONFIG:Release>:HERMES_ENABLE_PROFILING>)
-    target_compile_definitions(${name} PRIVATE HERMES_BUILD_ENGINE)
-    target_compile_definitions(${name} PRIVATE HERMES_APPLICATION_NAME="${HERMES_GAME_NAME}")
+    target_compile_definitions(${name} PRIVATE HERMES_APPLICATION_NAME="${HERMES_APPLICATION_NAME}")
 
+    source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${sources})
+endfunction()
+
+function(add_engine_sublib name sources)
+    add_hermes_sublib(${name} "${sources}")
+
+    target_compile_definitions(${name} PRIVATE HERMES_BUILD_ENGINE)
     target_include_directories(${name} PUBLIC ${HERMES_SOURCE_DIR})
 
     # FIXME: this is a bit ugly :(
     target_link_libraries(${name} PUBLIC $<$<CONFIG:Release>:Tracy::TracyClient>)
     target_include_directories(${name} PRIVATE "${HERMES_THIRD_PARTY_DIR}/VulkanMemoryAllocator/include")
     target_link_libraries(${name} PRIVATE Vulkan::Vulkan)
+endfunction()
 
-    source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR} FILES ${sources})
 endfunction()
